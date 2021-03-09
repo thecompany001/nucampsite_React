a9a28cf93030fb9456886b9e9f3a523ef6e5dd4b
the company <thecompany001@protonmail.com>
2021-03-09 16:22:58 -0500

    Rearrange files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
+import Main from './components/MainComponent';
 import './App.css';
 
 class App extends Component {
-  render() {
-      return (
-          <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory />
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Main />
+            </div>
+        );
+    };
 }
+
 
 export default App;
