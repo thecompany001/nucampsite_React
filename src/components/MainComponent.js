d1f8b3c5df3f77c20ed196010dc9ea89107998fd
the company <thecompany001@protonmail.com>
2021-03-16 22:00:15 -0400

    Rendered AboutComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -52,7 +52,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route path='/about' component={About} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
