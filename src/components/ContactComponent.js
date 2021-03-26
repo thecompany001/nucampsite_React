bd95dcc8792402efe73421a63698291779786323
the company <thecompany001@protonmail.com>
2021-03-25 23:54:59 -0400

    test

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -205,7 +205,7 @@ class Contact extends Component {
                                         className="form-control"
                                     />
                                 </Col>
-                            </Ro>
+                            </Row>
                             <Row className="form-group">
                                 <Col md={{size: 10, offset: 2}}>
                                     <Button type="submit" color="primary">
